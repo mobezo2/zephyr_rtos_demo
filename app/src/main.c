1c8dabfaccad1bbb7d06f4754d09ec0f72ebbd9a
Bezold, Max <max.bezold@gmail.com>
2022-03-05 22:24:46 -0700

    Updated example files for my own example project - initially a faster blinky with some continuous console output

--- app/src/main.c
+++ app/src/main.c
@@ -1,47 +1,49 @@
 /*
- * Copyright (c) 2021 Nordic Semiconductor ASA
+ * Copyright (c) 2016 Intel Corporation
+ *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr.h>
-#include <drivers/sensor.h>
+#include <drivers/gpio.h>
+#include <sys/printk.h>
 
-#include "app_version.h"
+/* 1000 msec = 1 sec */
+#define SLEEP_TIME_MS   500
 
-#include <logging/log.h>
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+/* The devicetree node identifier for the "led0" alias. */
+#define LED0_NODE DT_ALIAS(led0)
+
+/*
+ * A build error on this line means your board is unsupported.
+ * See the sample documentation for information on how to fix this.
+ */
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 void main(void)
 {
 	int ret;
-	const struct device *sensor;
+	int counter = 0;
 
-	printk("Zephyr Example Application %s\n", APP_VERSION_STR);
-
-	sensor = DEVICE_DT_GET(DT_NODELABEL(examplesensor0));
-	if (!device_is_ready(sensor)) {
-		LOG_ERR("Sensor not ready");
+	if (!device_is_ready(led.port)) {
 		return;
 	}
 
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return;
+	}
+
+	printk("Hello World! %s\n", CONFIG_BOARD);
+
 	while (1) {
-		struct sensor_value val;
-
-		ret = sensor_sample_fetch(sensor);
+		ret = gpio_pin_toggle_dt(&led);
 		if (ret < 0) {
-			LOG_ERR("Could not fetch sample (%d)", ret);
 			return;
 		}
+		k_msleep(SLEEP_TIME_MS);
+		counter++;
+		printk("Current count is %i\n", counter);
 
-		ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
-		if (ret < 0) {
-			LOG_ERR("Could not get sample (%d)", ret);
-			return;
-		}
-
-		printk("Sensor value: %d\n", val.val1);
-
-		k_sleep(K_MSEC(1000));
 	}
 }
-
