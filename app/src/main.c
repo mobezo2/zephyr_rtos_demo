d4456a921be694b8387ead8760af0b72791f3a58
Bezold, Max <max.bezold@gmail.com>
2022-03-11 16:12:47 -0700

    new config files and source code for mmc fat fs testing

--- app/src/main.c
+++ app/src/main.c
@@ -7,12 +7,36 @@
 #include <zephyr.h>
 #include <drivers/gpio.h>
 #include <sys/printk.h>
+#include <device.h>
+#include <storage/disk_access.h>
+#include <logging/log.h>
+#include <fs/fs.h>
+#include <ff.h>
+
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   500
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
+
+
+LOG_MODULE_REGISTER(main);
+
+static int lsdir(const char *path);
+
+static FATFS fat_fs;
+/* mounting info */
+static struct fs_mount_t mp = {
+	.type = FS_FATFS,
+	.fs_data = &fat_fs,
+};
+
+/*
+*  Note the fatfs library is able to mount only strings inside _VOLUME_STRS
+*  in ffconf.h
+*/
+static const char *disk_mount_pt = "/SD:";
 
 /*
  * A build error on this line means your board is unsupported.
@@ -24,6 +48,10 @@ void main(void)
 {
 	int ret;
 	int counter = 0;
+	static const char *disk_pdrv = "SD";
+	uint64_t memory_size_mb;
+	uint32_t block_count;
+	uint32_t block_size;
 
 	if (!device_is_ready(led.port)) {
 		return;
@@ -41,9 +69,91 @@ void main(void)
 		if (ret < 0) {
 			return;
 		}
-		k_msleep(SLEEP_TIME_MS);
+
 		counter++;
 		printk("Current count is %i\n", counter);
 
+		if (disk_access_init(disk_pdrv) != 0) {
+			LOG_ERR("Storage init ERROR!");
+			break;
+		}
+
+		if (disk_access_ioctl(disk_pdrv,
+				DISK_IOCTL_GET_SECTOR_COUNT, &block_count)) {
+			LOG_ERR("Unable to get sector count");
+			break;
+		}
+		LOG_INF("Block count %u", block_count);
+
+		if (disk_access_ioctl(disk_pdrv,
+				DISK_IOCTL_GET_SECTOR_SIZE, &block_size)) {
+			LOG_ERR("Unable to get sector size");
+			break;
+		}
+		printk("Sector size %u\n", block_size);
+
+		memory_size_mb = (uint64_t)block_count * block_size;
+		printk("Memory Size(MB) %u\n", (uint32_t)(memory_size_mb >> 20));
+
+		k_msleep(SLEEP_TIME_MS);
 	}
+
+
+	// Execute the following in case we break out of the disk code
+	mp.mnt_point = disk_mount_pt;
+
+	int res = fs_mount(&mp);
+
+	if (res == FR_OK) {
+		printk("Disk mounted.\n");
+		lsdir(disk_mount_pt);
+	} else {
+		printk("Error mounting disk.\n");
+	}
+
+	while (1) {
+		k_sleep(K_MSEC(1000));
+	}
+
+
+
+}
+
+static int lsdir(const char *path)
+{
+	int res;
+	struct fs_dir_t dirp;
+	static struct fs_dirent entry;
+
+	fs_dir_t_init(&dirp);
+
+	/* Verify fs_opendir() */
+	res = fs_opendir(&dirp, path);
+	if (res) {
+		printk("Error opening dir %s [%d]\n", path, res);
+		return res;
+	}
+
+	printk("\nListing dir %s ...\n", path);
+	for (;;) {
+		/* Verify fs_readdir() */
+		res = fs_readdir(&dirp, &entry);
+
+		/* entry.name[0] == 0 means end-of-dir */
+		if (res || entry.name[0] == 0) {
+			break;
+		}
+
+		if (entry.type == FS_DIR_ENTRY_DIR) {
+			printk("[DIR ] %s\n", entry.name);
+		} else {
+			printk("[FILE] %s (size = %zu)\n",
+				entry.name, entry.size);
+		}
+	}
+
+	/* Verify fs_closedir() */
+	fs_closedir(&dirp);
+
+	return res;
 }
